e42ab142d2f605cdb44d8922153377e50bc2d656
bnazarbekov <bnazarbekov@psystems.biz>
2024-09-27 04:10:01 +0600

    updated entpoint

--- app/Services/Product/ProductService.php
+++ app/Services/Product/ProductService.php
@@ -8,24 +8,127 @@ class ProductService
 {
     public function getProductDetailsBySerialNumber(string $serialNumber): array
     {
-        return DB::table('tbl_base_product as p')
-            ->leftJoin('tbl_master_data_values as cpu', 'p.cpu_id', '=', 'cpu.value_id')
-            ->leftJoin('tbl_master_data_values as ssd', 'p.ssd_id', '=', 'ssd.value_id')
-            ->leftJoin('tbl_master_data_values as hdd', 'p.ssd_id', '=', 'hdd.value_id')
-            ->leftJoin('tbl_master_data_values as ram', 'p.ram_id', '=', 'ram.value_id')
-            ->leftJoin('tbl_master_data_values as os', 'p.os_id', '=', 'os.value_id')
-            ->leftJoin('tbl_sb_serial_numbers as sn', 'p.id', '=', 'sn.product_id')
-            ->select(
-                'p.display_title',
-                'sn.serial_number',
-                'cpu.name as cpu_name',
-                'ssd.name as ssd_name',
-                'hdd.name as hdd_name',
-                'ram.name as ram_name',
-                'os.name as os_name'
-            )
-            ->where('sn.serial_number', $serialNumber)
-            ->get()
-            ->toArray();
+        $serialNumberValue = $this->getSerialNumberValue($serialNumber);
+
+        if (!$serialNumberValue) {
+            return [];
+        }
+
+        $order = $this->getOrderDetails($serialNumberValue->serial_id);
+
+        $result = [
+            'serial_number' => $serialNumber,
+            'raid' => $this->getRaidFromOrder($order)
+        ];
+
+        $productDetails = $order ? $this->getKitOrProductDetails($order->sku, $serialNumberValue->product_id)
+            : $this->getBaseProductDetailsById($serialNumberValue->product_id);
+
+        return array_merge($result, $productDetails);
+    }
+
+    private function getSerialNumberValue(string $serialNumber)
+    {
+        return DB::table("tbl_sb_serial_numbers")
+            ->where('serial_number', $serialNumber)
+            ->select('serial_id', 'product_id')
+            ->first();
+    }
+
+    private function getOrderDetails(string $serialId)
+    {
+        $order = DB::table('tbl_sb_serial_number_location_change_history as history')
+            ->leftJoin('tbl_sb_order_items as item', 'item.orderid', '=', 'history.shipped_location_order_id')
+            ->select('item.productid as sku', 'item.orderid as order_id')
+            ->where('history.serial_id', $serialId)
+            ->where('history.shipped_location_order_id', '!=', 0)
+            ->whereNotNull('item.id')
+            ->first();
+
+        return $order ?: DB::table('tbl_sb_serial_number_location_change_history as history')
+            ->leftJoin('tbl_sb_history_order_item as item', 'item.orderid', '=', 'history.shipped_location_order_id')
+            ->select('item.sku as sku', 'item.orderid as order_id')
+            ->where('history.serial_id', $serialId)
+            ->where('history.shipped_location_order_id', '!=', 0)
+            ->whereNotNull('item.id')
+            ->first();
+    }
+
+    private function getRaidFromOrder($order)
+    {
+        return $order ? $this->getRaidData($order->order_id)?->Raid : null;
+    }
+
+    private function getRaidData(string $orderId)
+    {
+        return DB::table('tbl_sb_qc_data')->where('orderid', $orderId)->select('Raid')->first();
+    }
+
+    private function getBaseProductDetailsById(int $productId): array
+    {
+        $baseProduct = DB::table('tbl_base_product')
+            ->where('id', $productId)
+            ->select('system_title', 'display_title')
+            ->first();
+
+        return $this->extractProductDetails($baseProduct);
+    }
+
+    private function getKitOrProductDetails(string $sku, int $productId): array
+    {
+        $sku = $this->normalizeSku($sku);
+        $kitData = $this->getKitData($sku);
+
+        return $kitData ?: $this->getBaseProductDetailsById($productId);
+    }
+
+    private function normalizeSku(string $sku): string
+    {
+        if (str_starts_with($sku, 'B')) {
+            $bundleSku = DB::table('tbl_kit_bundle_relation')
+                ->select('kit_sku')
+                ->where('sc_bundle_kit_sku', $sku)
+                ->orWhere('bundle_kit_sku', $sku)
+                ->first();
+
+            return $bundleSku ? str_replace(['-GPT', '-CA'], '', $bundleSku->kit_sku) : $sku;
+        }
+
+        return str_replace('-GPT', '', $sku);
+    }
+
+    private function getKitData(string $sku): ?array
+    {
+        $kitValue = DB::table('tbl_kit')
+            ->where('kit_sku', $sku)
+            ->select('kit_display_title', 'kit_ram_title', 'kit_storage_title', 'kit_gpu_title', 'kit_os_title', 'kit_cpu_title')
+            ->first()
+            ?? DB::table('tbl_kit_archive')
+                ->where('kit_sku', $sku)
+                ->select('kit_display_title', 'kit_ram_title', 'kit_storage_title', 'kit_gpu_title', 'kit_os_title', 'kit_cpu_title')
+                ->first();
+
+        return $kitValue ? [
+            'display_title' => $kitValue->kit_display_title,
+            'ram' => $kitValue->kit_ram_title,
+            'storage' => $kitValue->kit_storage_title,
+            'gpu' => $kitValue->kit_gpu_title,
+            'os' => $kitValue->kit_os_title,
+            'cpu' => $kitValue->kit_cpu_title,
+        ] : null;
+    }
+
+    private function extractProductDetails(object $productDetails): array
+    {
+        $productValues = explode(':||:', $productDetails->system_title);
+
+        return [
+            'display_title' => $productDetails->display_title,
+            'ram' => $productValues[2] ?? null,
+            'storage' => $productValues[3] ?? null,
+            'gpu' => $productValues[5] ?? null,
+            'os' => $productValues[6] ?? null,
+            'cpu' => $productValues[1] ?? null,
+        ];
     }
 }
